273a5ee317f1a21735cb8c2b924c159873f13300
Xinos <userxinos@gmail.com>
2021-04-15 16:11:34 +0700

    init v1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hot remount
+
+<img src="preview/image0.png" width="700px" align="center" alt="Hot remount Preview">
+
+New command to track changes in the plugin and automatic remount it
+
+using: `.watch <plugin-id>`
---
+++ index.js
@@ -0,0 +1,85 @@
+const { Plugin } = require('powercord/entities');
+const { watch } = require('fs');
+
+module.exports = class HotRemount extends Plugin {
+  async startPlugin () {
+    this.registerCommand();
+    powercord.once('loaded', this.onLoaded.bind(this));
+  }
+
+  pluginWillUnload () {
+    powercord.api.commands.unregisterCommand('watch');
+  }
+
+  onLoaded () {
+    this.settings.get('plugins', []).forEach((id) => this.watch.start(id));
+  }
+
+  registerCommand () {
+    powercord.api.commands.registerCommand({
+      command: 'watch',
+      label: 'Hot remount',
+      usage: '{c} < plugin-id >',
+      description: 'Track changes in the plugin and automatic remount it',
+      executor: this.run.bind(this),
+      autocomplete: this.autocomplete.bind(this)
+    });
+  }
+
+  run ([ id ]) {
+    if (powercord.pluginManager.plugins.has(id)) {
+      if (this.settings.get('plugins', []).includes(id)) {
+        return { result: 'Already watched' };
+      }
+      this.watch.start(id);
+      return false;
+    }
+    return { result: 'Plugin not found' };
+  }
+
+  autocomplete ([ findId, ...args ]) {
+    if (args.length) {
+      return;
+    }
+    return {
+      commands: [ ...powercord.pluginManager.plugins ]
+        .filter(([ id ]) => id.includes(findId))
+        .map(([ id ]) => ({ command: id })),
+      header: 'Plugins list'
+    };
+  }
+
+  get watch () {
+    const { settings, notice } = this;
+    const plugins = settings.get('plugins', []);
+
+    return {
+      start (id) {
+        const plugin = powercord.pluginManager.plugins.get(id);
+        const watcher = watch(plugin.entityPath, { recursive: true }, global._.debounce(() => {
+          powercord.pluginManager.remount(id);
+        }, 250));
+
+        settings.set('plugins', [ ...plugins, id ]);
+        notice(id, plugin.manifest.name, () => this.stop(id, watcher));
+      },
+      stop (id, watcher) {
+        watcher.close();
+        plugins.splice(plugins.indexOf(id), 1);
+        settings.set('plugins', plugins);
+      }
+    };
+  }
+
+  notice (id, name, onClick) {
+    powercord.api.notices.sendAnnouncement(`hot-remount-stop-${id}`, {
+      message: `Watch the plugin "${name}"`,
+      button: {
+        text: 'Stop',
+        onClick
+      }
+    });
+  }
+};
+
+
